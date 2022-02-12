9cd1d8b7b20071fdf42a6fc9c0fd2d97d4d0e66f
midoushitongtong <1092879991@qq.com>
2022-02-12 22:21:06 +0800

    ...

---
+++ src/components/header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <!-- content -->
+    <div class="content">
+      <span class="icon"></span>
+      <h1 class="text">Chicken Music</h1>
+    </div>
+    <!-- user icon -->
+    <RouterLink
+      class="mine"
+      :to="{
+        name: 'User',
+      }"
+    >
+      <i class="icon-mine"></i>
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss" scoped>
+@import './Header';
+</style>
---
+++ src/views/user/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">user</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'User',
+});
+</script>
+
+<style lang="scss" scoped>
+@import './User';
+</style>
